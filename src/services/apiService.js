1e9d34cab148b892bfd5093eb98ee657901f8011
Nishanth Jadav <nishanthjadav@gmail.com>
2025-01-08 21:52:52 -0500

    Update apiService.js

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -14,8 +14,13 @@ export const fetchData = async (filters, sortBy) => {
         queryParams.set('sortBy', sortBy); // Append the sorting criteria to the query parameters
 
         // Send GET request to the API endpoint with the query parameters
-        const response = await fetch(`${API_URL}?${queryParams.toString()}`);
-
+        const response = await fetch(`${API_URL}?${queryParams.toString()}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            mode: 'cors', // Ensure CORS mode is enabled
+        });
         // If the response is not OK (status code not in the range 200-299), throw an error
         if (!response.ok) {
             throw new Error("Failed to fetch data");
